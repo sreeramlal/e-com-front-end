b13ea8e878de4b16a74cd9a36d816c56b980d00c
sreeramlal <sreeramlalvp123@gmail.com>
2024-03-30 16:22:39 +0530

    add initial code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-extrabold underline">
+      Ecomm Front-end
+    </div>
+  );
+}
+
+export default App;
